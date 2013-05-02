aa76f37674a2e18f4744e00f7336edfc30fbab1b
Charles F. Munat <charles.munat@gmail.com>
2013-05-02 16:40:19 +0100

    Added basic app and client views. Deleted old templates and moved auto_row to a separate template.

---
+++ app/assets/javascripts/views/app_views.js
@@ -0,0 +1,15 @@
+BridgeCity.AppRowView = Backbone.View.extend({
+  el: '#insert_view_here'
+});
+
+BridgeCity.AppListView = Backbone.View.extend({
+  el: '#insert_view_here'
+});
+
+BridgeCity.AppDetailView = Backbone.View.extend({
+  el: '#insert_view_here'
+});
+
+BridgeCity.AppFormView = Backbone.View.extend({
+  el: '#insert_view_here'
+});
---
+++ app/assets/javascripts/views/client_views.js
@@ -0,0 +1,15 @@
+BridgeCity.ClientRowView = Backbone.View.extend({
+  el: '#insert_view_here'
+});
+
+BridgeCity.ClientListView = Backbone.View.extend({
+  el: '#insert_view_here'
+});
+
+BridgeCity.ClientDetailView = Backbone.View.extend({
+  el: '#insert_view_here'
+});
+
+BridgeCity.ClientFormView = Backbone.View.extend({
+  el: '#insert_view_here'
+});
